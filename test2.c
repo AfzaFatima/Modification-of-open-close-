6aba558bac2a7c43b948901127e03d67d6040b67
AfzaFatima <134057148+AfzaFatima@users.noreply.github.com>
2023-05-19 15:49:43 +0500

    Add files via upload

---
+++ test2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h> 
+#include<sys/syscall.h>
+int main(){ 
+	int fd, bread; 
+	char buffer[256];
+	fd=syscall(335,"test.txt",O_RDONLY);
+	if(fd==-1){
+		perror("myopen");
+		exit(EXIT_FAILURE);
+	}
+	bread=read(fd, buffer,256);
+	if(read ==-1){
+		perror("read");
+		exit(EXIT_FAILURE);
+	}
+	printf("%.*s",read,buffer);
+	printf("\n");
+	syscall(335,fd);
+	return 0;
+}
